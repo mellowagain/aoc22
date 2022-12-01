004bf6bfddb18926420b13a4858be2ea438f1cde
Mari <git@cutegirl.tech>
2022-12-01 18:18:00 +0100

    rust fmt and compute a before b

--- src/day1/main.rs
+++ src/day1/main.rs
@@ -3,12 +3,19 @@ use itertools::Itertools;
 fn main() {
     let input = include_str!("input.txt");
 
-    let mut vec: Vec<i32> = input.split("\n\n")
-        .map(|elf| elf.lines().map(|calories| calories.parse::<i32>().unwrap()).sum())
+    let mut vec: Vec<i32> = input
+        .split("\n\n")
+        .map(|elf| {
+            elf.lines()
+                .map(|calories| calories.parse::<i32>().unwrap())
+                .sum()
+        })
         .collect();
 
-    let b: i32 = (0..3).map(|_| vec.remove(vec.iter().position_max().unwrap())).sum();
-    let a = vec.into_iter().max().unwrap();
+    let a = *vec.iter().max().unwrap();
+    let b: i32 = (0..3)
+        .map(|_| vec.remove(vec.iter().position_max().unwrap()))
+        .sum();
 
     println!("part a: {}", a);
     println!("part b: {}", b);
