ce3601dc0144afffe3d4ac88a6bbd55650e0476d
Mari <git@cutegirl.tech>
2022-12-01 17:32:34 +0100

    immediately drop unused variable

--- src/day1/main.rs
+++ src/day1/main.rs
@@ -7,7 +7,7 @@ fn main() {
         .map(|elf| elf.lines().map(|calories| calories.parse::<i32>().unwrap()).sum())
         .collect();
 
-    let b: i32 = (0..3).map(|a| vec.remove(vec.iter().position_max().unwrap())).sum();
+    let b: i32 = (0..3).map(|_| vec.remove(vec.iter().position_max().unwrap())).sum();
     let a = vec.into_iter().max().unwrap();
 
     println!("part a: {}", a);
